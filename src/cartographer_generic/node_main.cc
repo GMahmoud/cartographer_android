12641fa396b66f8afcecbecfb4f10a56a815e59e
G_Mahmoud <mahmoud.ghorbel@awabot.com>
2017-09-12 09:55:49 +0200

    Getting submaps' pose from SubmapEntry

--- src/cartographer_generic/node_main.cc
+++ src/cartographer_generic/node_main.cc
@@ -40,8 +40,8 @@ string configuration_basename = "buddy_lidar_2d.lua";
 ::cartographer_generic_msgs::SubmapQuery::Response response;
 
 /*******************************************************************************
-* - Lua Configuration Load System
-* *****************************************************************************/
+ * - Lua Configuration Load System
+ * *****************************************************************************/
 std::tuple<NodeOptions, TrajectoryOptions> LoadOptions() {
 	auto file_resolver = cartographer::common::make_unique<
 			cartographer::common::ConfigurationFileResolver>(
@@ -62,40 +62,40 @@ void _LoadLua(const char*basename, int size_basename, const char* code, int size
 }
 
 /*******************************************************************************
-* - Init Cartographer
-********************************************************************************/
+ * - Init Cartographer
+ ********************************************************************************/
 Node* _Init() {
 	constexpr double kTfBufferCacheTimeInSeconds = 1e6;
 
-    //Init LaserScan msg constants
-    laser_msg.reset(new cartographer_generic_msgs::LaserScan());
-    laser_msg->ranges.reserve(360);
-    laser_msg->header.seq = 1;
-    laser_msg->header.frame_id = "buddy_tablet";
-    laser_msg->angle_min=-1.57079637051;
-    laser_msg->angle_max=1.57079637051;
-    laser_msg->angle_increment=0.00872664619237;
-    laser_msg->time_increment=0.0;
-    laser_msg->scan_time=0.0333333350718;
-    laser_msg->range_min=0.449999988079;
-    laser_msg->range_max=6.0;
+	//Init LaserScan msg constants
+	laser_msg.reset(new cartographer_generic_msgs::LaserScan());
+	laser_msg->ranges.reserve(360);
+	laser_msg->header.seq = 1;
+	laser_msg->header.frame_id = "buddy_tablet";
+	laser_msg->angle_min=-1.57079637051;
+	laser_msg->angle_max=1.57079637051;
+	laser_msg->angle_increment=0.00872664619237;
+	laser_msg->time_increment=0.0;
+	laser_msg->scan_time=0.0333333350718;
+	laser_msg->range_min=0.449999988079;
+	laser_msg->range_max=6.0;
 
-    //Init Odometry msg constants
-    odom_msg.reset(new cartographer_generic_msgs::Odometry());
-    odom_msg->header.seq=1;
-    odom_msg->header.frame_id = "world";
-    odom_msg->child_frame_id = "buddy";
+	//Init Odometry msg constants
+	odom_msg.reset(new cartographer_generic_msgs::Odometry());
+	odom_msg->header.seq=1;
+	odom_msg->header.frame_id = "world";
+	odom_msg->child_frame_id = "buddy";
 
-    //TODO
-    //  tf2_ros::Buffer tf_buffer{::ros::Duration(kTfBufferCacheTimeInSeconds)};
-    //  tf2_ros::TransformListener tf(tf_buffer);
+	//TODO
+	//  tf2_ros::Buffer tf_buffer{::ros::Duration(kTfBufferCacheTimeInSeconds)};
+	//  tf2_ros::TransformListener tf(tf_buffer);
 
-    NodeOptions node_options;
-    TrajectoryOptions trajectory_options;
-    std::tie(node_options, trajectory_options) = LoadOptions();
+	NodeOptions node_options;
+	TrajectoryOptions trajectory_options;
+	std::tie(node_options, trajectory_options) = LoadOptions();
 
-    //TODO FCA-MGO : check tf_buffer use !
-    Node* node = new Node(node_options/*, &tf_buffer*/);
+	//TODO FCA-MGO : check tf_buffer use !
+	Node* node = new Node(node_options/*, &tf_buffer*/);
 	//TODO If we want to add a primary map
 	//	if (!FLAGS_map_filename.empty()) {
 	//		node.LoadMap(FLAGS_map_filename);
@@ -107,66 +107,66 @@ Node* _Init() {
 void _Stop (Node* node) {
 	LOG(INFO) << "_Stop(Node*) Begins" ;
 	node->FinishAllTrajectories();
-    //TODO FCA-MGO : this must be done sometime ! Check wether it should be here
-    //or somewhere else (FinishAllTrajectories use ?)
-    //delete node;
-    //node = nullptr;
+	//TODO FCA-MGO : this must be done sometime ! Check wether it should be here
+	//or somewhere else (FinishAllTrajectories use ?)
+	//delete node;
+	//node = nullptr;
 	LOG(INFO) << "_Stop(Node*) Ends" ;
 }
 
 /*******************************************************************************
-* - Main callback handler : LaserScan, Odometry, IMU...
-********************************************************************************/
+ * - Main callback handler : LaserScan, Odometry, IMU...
+ ********************************************************************************/
 void _LaserScanCallback(Node* node, float* ranges, int64 time) {
-    laser_msg->ranges.clear(); //clear does not resize
-    for(int i=0; i<360; i++) //1° definition - LaserScan data
-	   laser_msg->ranges.push_back(ranges[i]) ;
-    //Convert time ticks in common time
-    laser_msg->header.stamp = ::cartographer::common::FromUniversal(time);
+	laser_msg->ranges.clear(); //clear does not resize
+	for(int i=0; i<360; i++) //1° definition - LaserScan data
+		laser_msg->ranges.push_back(ranges[i]) ;
+	//Convert time ticks in common time
+	laser_msg->header.stamp = ::cartographer::common::FromUniversal(time);
 
-    node->LaserScanCallback(laser_msg, trajectory_id);
-    //Increment header sequence
-    laser_msg->header.seq++;
+	node->LaserScanCallback(laser_msg, trajectory_id);
+	//Increment header sequence
+	laser_msg->header.seq++;
 }
 
 /*******************************************************************************
-* - Expecting pose, twist and covariance in the following format :
-* - pose as [x,y,z] + quat[x,y,z,w] and covariance pose [36]
-* - twist as [x,y,z] + [x,y,z] and covariance twist [36]
-********************************************************************************/
+ * - Expecting pose, twist and covariance in the following format :
+ * - pose as [x,y,z] + quat[x,y,z,w] and covariance pose [36]
+ * - twist as [x,y,z] + [x,y,z] and covariance twist [36]
+ ********************************************************************************/
 void _OdometryCallback(Node* node, float* pose, float* pose_covariance, float* twist, float* twist_covariance, int64 time) {
 
-    odom_msg->pose.pose.position.x = pose[0];
-    odom_msg->pose.pose.position.y = pose[1];
-    odom_msg->pose.pose.position.z = pose[2];
-    odom_msg->pose.pose.orientation.x = pose[3];
-    odom_msg->pose.pose.orientation.x = pose[4];
-    odom_msg->pose.pose.orientation.y = pose[5];
-    odom_msg->pose.pose.orientation.w = pose[6];
+	odom_msg->pose.pose.position.x = pose[0];
+	odom_msg->pose.pose.position.y = pose[1];
+	odom_msg->pose.pose.position.z = pose[2];
+	odom_msg->pose.pose.orientation.x = pose[3];
+	odom_msg->pose.pose.orientation.x = pose[4];
+	odom_msg->pose.pose.orientation.y = pose[5];
+	odom_msg->pose.pose.orientation.w = pose[6];
 
-    odom_msg->twist.twist.linear.x = twist[0];
-    odom_msg->twist.twist.linear.y = twist[1];
-    odom_msg->twist.twist.linear.z = twist[2];
-    odom_msg->twist.twist.angular.x = twist[3];
-    odom_msg->twist.twist.angular.y = twist[4];
-    odom_msg->twist.twist.angular.z = twist[5];
+	odom_msg->twist.twist.linear.x = twist[0];
+	odom_msg->twist.twist.linear.y = twist[1];
+	odom_msg->twist.twist.linear.z = twist[2];
+	odom_msg->twist.twist.angular.x = twist[3];
+	odom_msg->twist.twist.angular.y = twist[4];
+	odom_msg->twist.twist.angular.z = twist[5];
 
-    for(int i=0 ; i<36 ; i++){
-        odom_msg->pose.covariance[i]=pose_covariance[i];
-        odom_msg->twist.covariance[i]=twist_covariance[i];
-    }
+	for(int i=0 ; i<36 ; i++){
+		odom_msg->pose.covariance[i]=pose_covariance[i];
+		odom_msg->twist.covariance[i]=twist_covariance[i];
+	}
 
-    odom_msg->header.stamp = ::cartographer::common::FromUniversal(time);
+	odom_msg->header.stamp = ::cartographer::common::FromUniversal(time);
 
-    node->OdometryCallback(odom_msg, trajectory_id);
+	node->OdometryCallback(odom_msg, trajectory_id);
 
-    odom_msg->header.seq++;
+	odom_msg->header.seq++;
 }
 
 /*******************************************************************************
-* - Submap Query/Retrieve
-* HandleSubmapQuery must be call first.
-********************************************************************************/
+ * - Submap Query/Retrieve
+ * HandleSubmapQuery must be call first.
+ ********************************************************************************/
 int _HandleSubmapQuery(Node* node){
 	::cartographer_generic_msgs::SubmapList SubmapList = node->GetSubmapList();
 	::cartographer_generic_msgs::SubmapQuery::Request request;
@@ -180,8 +180,8 @@ int _HandleSubmapQuery(Node* node){
 //Retrieve grid size (Unity should handle most of memory allocation)
 // response.cells.size() = width * height * 2 (intensity and alpha channels)
 void _GetGridSize(int* size){
-    size[0] = response.cells.size()!=0 ? response.width : -1;
-    size[1] = response.cells.size()!=0 ? response.height : -1;
+	size[0] = response.cells.size()!=0 ? response.width : -1;
+	size[1] = response.cells.size()!=0 ? response.height : -1;
 }
 
 double _GetGridResolution(){
@@ -189,15 +189,36 @@ double _GetGridResolution(){
 }
 
 //Pose format as [x,y,z] + quat[x,y,z,w]
-void _GetSubmapPose(double* pose)
+void _GetSubmapPose(Node* node, double* pose)
 {
-    pose[0] = response.cells.size()!=0 ? response.slice_pose.position.x : 0;
-    pose[1] = response.cells.size()!=0 ? response.slice_pose.position.y : 0;
-    pose[2] = response.cells.size()!=0 ? response.slice_pose.position.z : 0;
-    pose[3] = response.cells.size()!=0 ? response.slice_pose.orientation.x : 0;
-    pose[4] = response.cells.size()!=0 ? response.slice_pose.orientation.y : 0;
-    pose[5] = response.cells.size()!=0 ? response.slice_pose.orientation.z : 0;
-    pose[6] = response.cells.size()!=0 ? response.slice_pose.orientation.w : 0;
+	::cartographer_generic_msgs::SubmapList SubmapList = node->GetSubmapList();
+	::cartographer_generic_msgs::SubmapEntry LastSubEnt = SubmapList.submap.back();
+	pose[0] = LastSubEnt.pose.position.x ;
+	pose[1] = LastSubEnt.pose.position.y ;
+	pose[2] = LastSubEnt.pose.position.z ;
+	pose[3] = LastSubEnt.pose.orientation.x ;
+	pose[4] = LastSubEnt.pose.orientation.y ;
+	pose[5] = LastSubEnt.pose.orientation.z ;
+	pose[6] = LastSubEnt.pose.orientation.w ;
+
+	std::stringstream ss;
+	int size = SubmapList.submap.size();
+	for(int i=0; i<size; i++ ){
+		::cartographer_generic_msgs::SubmapEntry subEnt = SubmapList.submap[i];
+		ss << "Submap" << subEnt.submap_index << ": {" ;
+		ss << "Position (" << subEnt.pose.position.x << "," << subEnt.pose.position.y << "," << subEnt.pose.position.z << "), " ;
+		ss << "Orientation (" << subEnt.pose.orientation.x << "," << subEnt.pose.orientation.y << "," << subEnt.pose.orientation.z << "," << subEnt.pose.orientation.w << ")" ;
+		ss << "} \n";
+	}
+
+	int read=0;
+	std::string substr;
+	do {
+		substr = ss.str().substr(read,android_log_max_length);
+		LOG(INFO) << substr;
+		read+=substr.length();
+	} while(read<ss.str().length());
+
 }
 
 //Retrieve occupancy grid
@@ -205,19 +226,19 @@ void _GetSubmapPose(double* pose)
 void _GetOccupancyGrid (int* intensity, int* alpha) {
 	for (int i = 0; i < response.height; ++i) {
 		for (int j = 0; j < response.width; ++j) {
-  			intensity[i*response.width + j] = static_cast<int>(response.cells[(i * response.width + j) * 2]);
-  			alpha[i*response.width + j] = static_cast<int>(response.cells[(i * response.width + j) * 2 + 1]);
+			intensity[i*response.width + j] = static_cast<int>(response.cells[(i * response.width + j) * 2]);
+			alpha[i*response.width + j] = static_cast<int>(response.cells[(i * response.width + j) * 2 + 1]);
 		}
 	}
 }
 
 /*******************************************************************************
-* - Trajectory Query/Retrieve
-********************************************************************************/
+ * - Trajectory Query/Retrieve
+ ********************************************************************************/
 void _GetPose (Node* node, int64 time, float* pose){
 	::cartographer::common::Time time_now = ::cartographer::common::FromUniversal(time);
-    ::cartographer_generic_msgs::MarkerArray TrajectoryList = node->GetTrajectoryNodeList(time_now);
-    std::stringstream ss;
+	::cartographer_generic_msgs::MarkerArray TrajectoryList = node->GetTrajectoryNodeList(time_now);
+	std::stringstream ss;
 	for(int i=0 ; i<TrajectoryList.markers.size(); i++ ){
 		::cartographer_generic_msgs::Marker marker = TrajectoryList.markers.at(i);
 		ss << marker.id << ": {" ;
@@ -230,15 +251,14 @@ void _GetPose (Node* node, int64 time, float* pose){
 		}
 		ss << "} \n";
 	}
-    int read=0;
-    std::string substr;
-    do {
-        substr = ss.str().substr(read,android_log_max_length);
-        LOG(INFO) << substr;
-        read+=substr.length();
-    } while(read<ss.str().length());
+	int read=0;
+	std::string substr;
+	do {
+		substr = ss.str().substr(read,android_log_max_length);
+		LOG(INFO) << substr;
+		read+=substr.length();
+	} while(read<ss.str().length());
 }
-
 
 }  // namespace cartographer_generic
 }  // extern "C"
